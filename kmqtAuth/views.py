64706148cb5cfe289964fb1d019a04e95c9c6cf8
Ntkskwk <natsukawa247@outlook.com>
2024-03-17 22:23:23 +0800

    feat: generate url for activate

--- kmqtAuth/views.py
+++ kmqtAuth/views.py
@@ -99,12 +99,36 @@ class CreateKmqtUserViewSet(viewsets.ModelViewSet):
     queryset = KmqtUser.objects.all()
     serializer_class = CreateKmqtUserSerializer
     http_method_names = ['get', 'post']
+    permission_classes = []
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+
+        # Generate activation url
+        instance = KmqtUser.objects.get(username=request.data['username'])
+        activate_url = request.build_absolute_uri() + str(instance.uuid).replace("-", "")
+        print(activate_url)
+
+        instance.set_password(request.data['password'])
+        instance.save()
+
+        # Clear cache
+        if getattr(instance, '_prefetched_objects_cache', None):
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
     def retrieve(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(instance)
-        return Response(serializer.data)
+        instance = KmqtUser.objects.get(uuid=kwargs['pk'])
+        instance.is_activated = True
+        instance.save()
+        return Response(status=status.HTTP_200_OK)
 
+    def list(self, request, *args, **kwargs):
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
 
 class ProgramViewSet(viewsets.ModelViewSet):
